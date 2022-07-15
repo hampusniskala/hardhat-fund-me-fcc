959389bc4625ea5a34f06df5bb1d136a82bfc58f
WoodCollector <you@example.com>
2022-07-15 02:12:05 +0200

    initial commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,42 @@
+// function deployFunc() {
+//     console.log("test")
+// }
+
+// module.exports.default = deployFunc
+
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+
+    log("-----------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
---
+++ hardhat.config.js
@@ -0,0 +1,50 @@
+const { task } = require("hardhat/config")
+
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("./tasks/accounts")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    //solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+}
